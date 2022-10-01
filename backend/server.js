230d1b96c4f5e2d52ca2d9bb3ee3331ba99f01be
Y2theK <yeyintkyaw300@gmail.com>
2022-10-01 13:14:57 +0630

    install mongoose and connect with mongo/atlas databases

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,31 @@
 //require express
 const express = require("express");
+//require mongoose
+const mongoose = require("mongoose");
 //require dotenv config
 require("dotenv").config();
 //require workout routes
 const workoutRoutes = require("./routes/workouts");
 //initialize express app
 const app = express();
-//middleware
+//check req body and bind with req
+app.use(express.json());
+//global middleware
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 app.use("/api/workouts", workoutRoutes);
-//check req body and bind with req
-app.use(express.json());
-//routes
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome Node" });
-});
-//server start at port 4000
-app.listen(process.env.PORT, () => {
-  console.log("listening at port ", process.env.PORT);
-});
+
+//connect to mongo atlas database
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //server start at port 4000
+    app.listen(process.env.PORT, () => {
+      console.log("connected to db and listening at port ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log("connection error", error);
+  });
